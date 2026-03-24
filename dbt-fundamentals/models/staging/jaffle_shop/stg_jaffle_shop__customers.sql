6ed6ec162a83759838f63b487fe0ad4850914e9d
Geovanni Corsino <geovannicorsino@gmail.com>
2026-03-23 23:13:32 -0300

    feat/dbt-fundamentals: finish models module

---
+++ dbt-fundamentals/models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,6 @@
+SELECT 
+    id as customer_id,
+    first_name,
+    last_name,
+    
+FROM `geovanni-corsino-labs.jaffle_shop.customers`
---
+++ dbt-fundamentals/models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,7 @@
+SELECT 
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+    
+FROM `geovanni-corsino-labs.jaffle_shop.orders`
